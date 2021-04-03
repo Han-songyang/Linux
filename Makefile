239c4d0a955a8ecd9d9c3a054fad47a098bc30bc
Han-songyang <1838738883@qq.com>
2021-04-03 16:24:24 +0800

    4.3

---
+++ Makefile
@@ -0,0 +1,16 @@
+
+
+
+test.o:test.c
+	gcc -c $^ -o $@
+
+test1.o:test1.c
+	gcc -c $^ -o $@
+
+test2.o:test2.c
+	gcc -c $^ -o $@
+
+
+test: test.o test1.o test2.o
+	gcc $^ -o $@
+
---
+++ test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+    return a+b;
+}
+int main ()
+{
+    printf("hello bit\n");
+    int i = 0;
+    for ( i=0; i<5;i++)
+    {
+        printf("11111\n");
+    }
+    int a=0;
+    a=add(3,5);
+    printf("%d\n",a);
+    return 0;
+    
+}
